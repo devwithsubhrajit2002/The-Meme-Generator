b15d9a178ad62157c717b7de98d239f44f9898ff
Subhrajit Dhar <108873732+devwithsubhrajit2002@users.noreply.github.com>
2023-04-09 01:54:35 +0530

    Updated Header

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">Make all Laugh</h4>
         </header>
     )
 }
